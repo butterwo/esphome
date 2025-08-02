f77459c240c80c90d49a3bf21c8cf67ce1454c42
butterwo <butterwo@users.noreply.github.com>
2025-08-02 16:00:44 +0200

    Update __init__.py

--- components/rea131b/__init__.py
+++ components/rea131b/__init__.py
@@ -1,17 +1,14 @@
-import esphome.config_validation as cv
 import esphome.codegen as cg
-
+import esphome.config_validation as cv
 from esphome.const import CONF_ID
 
 rea131b_ns = cg.esphome_ns.namespace("rea131b")
-rea131b = rea131b_ns.class_("REA131B", cg.Component)
+REA131B = rea131b_ns.class_("REA131B", cg.Component)
 
 CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(rea131b),
-})
-
+    cv.GenerateID(): cv.declare_id(REA131B),
+ }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-
     await cg.register_component(var, config)
